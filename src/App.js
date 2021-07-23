da0689ec2dfac13a9b9993bf2d0bf077050ec512
JackMechem <mechemjack@gmail.com>
2021-07-22 19:25:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './comp/Header.js';
+import RecipieCatagory from './comp/RecipieCatagory'
+import styled from 'styled-components';
+import { Buttons, SearchBar } from './styles/elements';
+import { Container, Subhead } from './styles/global'
+import { AiOutlineSearch as SearchIcon } from 'react-icons/ai'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Container>
+        <RecipieCatagory catagoryName="Popular Recipies" />
+        <RecipieCatagory catagoryName="Top Rated Breakfasts" />
+        <RecipieCatagory catagoryName="Top Rated Lunches" />
+        <RecipieCatagory catagoryName="Top Rated Dinners" />
+        <RecipieCatagory catagoryName="Explore" />
+      </Container>
     </div>
   );
 }
 
+
+
+/* <Container>
+  <Subhead>Buttons</Subhead>
+  <div className="row">
+    <Buttons>Playlist</Buttons>
+    <Buttons outline>Playlist</Buttons>
+    <Buttons primary>Playlist</Buttons>
+    <Buttons secondary>Playlist</Buttons>
+  </div>
+  <Subhead>Search Bar</Subhead>
+  <div className="row">
+    <SearchBar>
+      <div>
+        <SearchIcon />
+      </div>
+
+      <input
+        placeholder="Search for cool things."
+        type="text"
+      />
+    </SearchBar>
+  </div>
+</Container> */
 export default App;
